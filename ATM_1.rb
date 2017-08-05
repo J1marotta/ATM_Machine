cdd6908b3521529d581d99c8b9a9eb26efd60608
J1marotta <jim_is55@hotmail.com>
2017-08-05 21:19:11 +1000

    added police and voice support

--- ATM_1.rb
+++ ATM_1.rb
@@ -16,31 +16,42 @@ def intro
 
   # greet the customer
   system("clear")
+  say("intro")
   puts
   puts
-  puts "Welcome to ATM MACHINE 10000"
-  puts "We are here to make your life amazing"
-  puts "And definitely not steal your identity"
+  puts "                     Welcome to ATM MACHINE 10000"
+  puts "                We are here to make your life amazing"
+  puts "                And definitely not steal your identity"
   puts
-  puts "I see you are a new bank customer"
-  puts "Please enter your details"
-  puts "Name"
+  puts "                I see you are a new bank customer"
+  puts "                    Please enter your details"
+  puts
+  print "                            Name: "
 
   # save their name
-  cName =gets.chomp.capitalize
-  puts "Hi #{cName} Welcome, now lets set up a pin"
+  cName =gets.capitalize
+  puts
+  puts "                             Hi #{cName}"
+  puts "                     Welcome, now lets set up a pin"
 
   # save their pin number
   @pinMaster = pin_control
 
 
   puts @pinMaster
-  puts "Okay Great"
+  puts
+  puts "                       Keep Your Pin Safe!"
   sleep(1)
   send_help
 # run the choice function
 choice
 end
+
+def balance
+
+
+end
+
 
 def choice
 =begin
@@ -54,15 +65,23 @@ the Intro code will keep running while runner is equal to 1.
  # run different methods depending on choice.
     case choice
     when "1"
+      say("2")
       balance
     when "2"
+      say("2")
       deposit
     when "3"
+      say("3")
       withdrawal
     when "4"
+      say("change pin")
       change_pin
     when "q"
+    #  say("quit")
       quit
+    when "h"
+      say("help")
+      send_help
     else
       send_help
     end
@@ -70,83 +89,161 @@ the Intro code will keep running while runner is equal to 1.
 end
 
 def change_pin
-  puts @pinMaster
+  puts "DEGBUG " +  @pinMaster
   system("clear")
   chances = 0
   while chances < 3
-    puts "Change Pin"
+    puts "                        Change PIN"
+    say("Change pin")
     puts
-    puts "Old PIN"
+    puts"                          Old PIN"
     old_p = gets.chomp
     if old_p != @pinMaster
-      puts  "Incorrect Pin"
+      puts "                     Incorrect Pin"
+      say("incorrect")
       chances += 1
     else
       puts @pinMaster
       # new_p = 0
-      puts "Enter your New pin"
+      puts "                    Enter your New pin"
       @pinMaster = pin_control
-      puts "PIN CHANGED"
+      puts "                      PIN CHANGED"
+      say("changed")
       chances = 0
       sleep(1)
       send_help
     break
     end
   end
-    puts "Three strikes, you're Out"
+    sleep(1)
+    puts "                    Three strikes, you're Out"
     puts
+
     police
 end
 
 
 def send_help
   system("clear")
-  puts "1    for DISPLAY BALANCE"
-  puts "2    for DEPOSIT"
-  puts "3    for WITHDRAWAL"
-  puts "4    to CHANGE PIN"
-  puts "h    for HELP"
-  puts "q    to QUIT"
+  say("help")
+  puts "                 1    for DISPLAY BALANCE"
+  puts "                 2    for DEPOSIT"
+  puts "                 3    for WITHDRAWAL"
+  puts "                 4    to CHANGE PIN"
+  puts "                 H    for HELP"
+  puts "                 Q    to QUIT"
   puts
 end
 
 def quit
   system("clear")
  puts
- puts "You can't quit yet we are buying stuff with your card in India"
- puts "hahaha, joking... "
- puts " ... "
- puts "Really Quit? [y/n]"
+ puts "      You can't quit yet we are buying stuff with your card in India"
+ puts "                       hahaha, joking... "
+ puts "                             ... "
+ puts "                      Really Quit? [y/n]"
  puts
  quit = gets.chomp.downcase
  case quit
  when "y"
    puts
-   puts "thanks for choosing ATM Machine 10000"
+   puts "              Thanks for choosing ATM Machine 10000"
    puts
    runner = 2
    exit
  when "n"
    puts
-   puts "Great choice, we are just hacking your facebook now"
+   puts "        Great choice, we are just hacking your facebook now"
    puts
-   help
+   send_help
  else
    puts
-   puts "Really quit? [y/n]"
+   puts "                      Really quit? [y/n]"
  end
 end
 
 def pin_control
   begin
     # @pin = pin
-    puts "Please enter a four digit pin"
+    puts "                   Please enter a four digit pin"
+    say("four")
     newPin = gets.chomp
   end while newPin.length != 4
   return newPin
   # @pinMaster = pin
 end
 
+def police
+# User has put in wrong pin three times, and now the police must be called
+
+
+# This block, displays a warning on the screen
+  5.times {
+    puts "                    WARNING WARNING WARNING"
+    puts "              : ! The police are on their way ! :"
+    puts "                    WARNING WARNING WARNING"
+    sleep(1)
+  }
+
+  # Pause for dramatic effect.
+  sleep (2)
+  say("police")
+  say("police")
+
+  # Bring the po po
+  File.open("police.txt", "r") do |f|
+    f.each_line do |line|
+      puts line
+    end
+  end
+  # end the program
+  runner = 2
+  exit
+end
+
+
+
+## New method to handle sounds
+def say(x)
+
+  case x
+  when "intro"
+    `say "Welcome to ATM MACHINE 10000."`
+
+  when "1"
+    `say "Display Balance"`
+
+  when "2"
+    `say "Make a Deposit"`
+
+  when "3"
+    `say "Make a Withdrawal"`
+
+  when "change pin"
+    `say "Change Pin"`
+
+  when "incorrect"
+    `say "Incorrect PIN"`
+
+  when "changed"
+    `say "Pin Successfully Change"`
+
+  when "police"
+    `say " YOU ARE UNDER ARREST"`
+
+  when "four"
+    `say "Please Enter a four Digit PIN"`
+
+  when "help"
+  `say "Push 1 for DISPLAY BALANCE"`
+  `say "Push 2 for Desposit"`
+  `say "Push 3 for Withdrawal"`
+  `say "Push 4 to change PIN"`
+  `say "Push H to Repeat the Help"`
+  `say "Push Q to QUIT"`
+
+  end
+end
 
 
 intro
